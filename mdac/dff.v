50b17722467eaa540b42d01668b2caebaf075688
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-01-08 06:48:52 +0800

    removed code, take time to study instead

--- mdac/dff.v
+++ mdac/dff.v
@@ -2,39 +2,20 @@ module dff (
     input  wire clk, //pendulum
     input  wire reset,
     input  wire d,
-    output reg  q
-); // this stores exactly bit
+    output reg  q, // should be a q'
+);
+ // this stores exactly bit
+// this uses the master slave
 
-    wire nd, gda, gdb, gdc, gdd, gm1, gm2, ;
+//“On the clock edge, store D. Otherwise, remember the old value.”
+//No illegal states, On every clock tick, become next_state[i], which will be implemented in fsm module
 
-    and_gate gd1(d, clk, gda);      // TODO: connect this to master latch properly (flip)
-    not_gate gd2(gda, gdb);         // TODO: check if this goes to slave latch (flop)
-
-    not_gate nd1(d, nd);                // TODO: use this inverted D correctly in your latch
-    and_gate gd3(nd, clk, gdc);         // TODO: connect to other part of the latch
-    not_gate gd4(gdc, gdd);             // TODO: complete feedback loop to hold value
-
-    and_gate m1(gdb,gm1,);
-    not_gate ma();
-    and_gate m2(gdd,gm2,gm1);
-    not_gate mb();
-
-    //on paper figure out how to nand this ^
+//we make 2 sr latches
+//Master	clk = 1	Captures D
+// Slave	clk = 0	Outputs stored value
 
 
 
-    always @(posedge clk) begin
-        // TODO: implement synchronous reset
-        // if (reset) q <= 0;
+//take time to study this
 
-        // TODO: assign q to the correct combination of your gate outputs
-        // q should only update on rising edge of clk
-        // q should hold its previous value until next clock edge
-    end
-
-    // 1. Make a master-slave latch using your gda/gdb/gdc/gdd wires
-    // 2. Connect the output of the slave latch to 'q'
-    // 3. Ensure q updates only on rising edge of clk
-    // 4. Implement synchronous reset: q = 0 when reset is high
-    // 5. Test the DFF with a simple clk and d waveform to ensure it stores value correctly
-endmodule
+//learn difference between dff and srff
